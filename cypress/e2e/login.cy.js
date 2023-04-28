5a15dde3524b22ff9c6394b7b3f7a221d38ac48f
IMEKAN ENANG <imekan@untapt.com>
2023-04-29 00:06:06 +0100

    Added Page object file and class

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,8 @@
 /// <reference types = "Cypress"/>
 
+import Login from "../PageObjects/Loginpage.js"
+
+
 describe('pom', () =>{
 
 //General approach
@@ -11,5 +14,16 @@ cy.get("button[type='submit']").click() //Locates the login button and click
 cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text', 'Dashboard');
 })
 
+//using Page object Model
+it.only ('loginTest', () =>{
+    cy.visit ("https://opensource-demo.orangehrmlive.com/") // Visits the URL
+
+    const ln=new Login();
+    ln.setUserName("Admin")
+    ln.setPassword("admin123")
+    ln.clickSubmit();
+    ln.verifyLogin();
+
+    })
 
 })
