a2126c80dc04556595585cf6cc6e90f9bf647ea5
IMEKAN ENANG <imekan@untapt.com>
2023-04-28 18:19:06 +0100

    Made changes to some locators on the login.js file

--- cypress/e2e/login-page.cy.js
+++ cypress/e2e/login-page.cy.js
@@ -6,7 +6,7 @@ describe('pom', () =>{
 it ('loginTest', () =>{
 cy.visit ("https://opensource-demo.orangehrmlive.com/")
 cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
-cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+cy.get("input[placeholder='Password']").type("admin123")
 cy.get("button[type='submit']").click() //Locates the login button and click
 cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text', 'Dashboard');
 })
